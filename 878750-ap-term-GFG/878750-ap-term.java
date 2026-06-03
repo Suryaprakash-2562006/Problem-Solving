acbdc309409c6deb72f87d3f0ed37490b5eb646d
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-03 21:43:46 +0530

    Added solution - LeetHub

---
+++ 878750-ap-term-GFG/878750-ap-term.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+
+        // code here
+        int an = a + (n-1) * d;
+        System.out.print(an);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
