e040569718e4942fd320fbc4d61073ac7d439092
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-03 21:39:58 +0530

    Added solution - LeetHub

--- 703954-prime-number-GFG/703954-prime-number.java
+++ 703954-prime-number-GFG/703954-prime-number.java
@@ -1,21 +1,20 @@
 class Solution {
     static boolean isPrime(int n) {
-        
-        
-        boolean isPrim = true;
-        
-        if(n == 1)
-        {
-            isPrim = false;
-        }
-        for(int i=2;i<n;i++)
-        {
-            if(n % i == 0)
-            {
-                isPrim = false;
+        // code here
+        boolean isPrime = true;
+        if(n == 1) return false;
+        for(int i=2;i<n;i++){
+            if(n % i == 0){
+                
+                isPrime = false;
+                break;
             }
         }
         
-        return isPrim;
+        return isPrime;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
