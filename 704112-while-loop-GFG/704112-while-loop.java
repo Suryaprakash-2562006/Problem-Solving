6ba639446cf73238200caec1f7433eaef3d136e0
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-03 21:42:14 +0530

    Added solution - LeetHub

---
+++ 704112-while-loop-GFG/704112-while-loop.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static void utility(int x) {
+        // code here
+        
+        while(x >=0){
+            System.out.print(x + " ");
+            x--;
+        }
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
