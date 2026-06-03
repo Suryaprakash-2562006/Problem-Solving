8c3c18ca3a249b349a40498067df1780dae53831
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-03 21:40:32 +0530

    Added solution - LeetHub

---
+++ 704102-swap-the-numbers-GFG/704102-swap-the-numbers.java
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public void swap(int a, int b) 
+    {
+        
+        int first = b;
+        int second = a;
+        
+
+        System.out.println(first + " " + second);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
