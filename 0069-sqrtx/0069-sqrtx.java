bef4387f09ff40a2a28ab6ccf30e75dcb22c474d
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-10 22:32:28 +0530

    Time: 20 ms (9.64%), Space: 41.9 MB (99.66%) - LeetHub - Time: O(?), Space: O(?)

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,7 +1,19 @@
 class Solution {
     public int mySqrt(int x) {
-        int sqrt = (int)Math.sqrt(x);
-        return sqrt;
+        /*int sqrt = (int)Math.sqrt(x);
+        return sqrt;*/
+
+        int ans = 0;
+
+        for(long i=1;i*i <= x;i++){
+            ans = (int)i;
+        }
+
+        return ans;
         
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
