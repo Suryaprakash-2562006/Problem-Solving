f3d16e9702d48277560d02a6c69547af5997e105
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-11 22:20:22 +0530

    Time: 8 ms (90.96%), Space: 46.1 MB (43.17%) - LeetHub - Time: O(log(n)), Space: O(1)

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+
+       if(n <= 0) return false;
+       if(n==1) return true;
+
+       long base = 1;
+
+       while(base < n){
+        base = base * 3;
+       }
+
+       return base==n;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
