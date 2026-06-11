3c865d2c9524fbd9e201541a192ce9c697a4f4be
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-11 22:17:14 +0530

    Time: 5 ms (62.33%), Space: 44.5 MB (60.27%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()) return false;
+
+        int count[] = new int[26];
+
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for(int c : count){
+            if(c != 0) return false;;
+        }
+
+        return true;
+
+        
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
