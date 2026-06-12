0a767f9d31859af37f4a99444be6c64fbf91b1ed
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-12 21:36:03 +0530

    Added solution - LeetHub

---
+++ 878729-print-with-space-GFG/878729-print-with-space.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public void utility(Scanner scn) {
+        String a = scn.nextLine();
+        String b = scn.nextLine();
+        
+        System.out.println(a + " " + b);
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
