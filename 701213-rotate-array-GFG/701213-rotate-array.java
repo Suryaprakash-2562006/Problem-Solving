3764e03840224704e652e0f9434a90a939402e04
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-16 22:47:00 +0530

    Added solution - LeetHub

---
+++ 701213-rotate-array-GFG/701213-rotate-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    static void rotateArr(int arr[], int d) {
+
+        d = d % arr.length;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+    }
+
+    static void reverse(int arr[], int start, int end) {
+
+        while (start < end) {
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
