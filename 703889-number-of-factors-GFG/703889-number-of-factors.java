c9058004c778be67c1c0af586fc9b393821978e8
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-17 21:22:16 +0530

    Added solution - LeetHub

---
+++ 703889-number-of-factors-GFG/703889-number-of-factors.java
@@ -0,0 +1,18 @@
+class Solution {
+    static int countFactors(int n) {
+        // code here
+        int count = 0;
+        
+        for(int i=1;i<=n;i++){
+            if(n % i == 0){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
