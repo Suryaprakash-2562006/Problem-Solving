09dcef84a7a131cf906205328ce9e094d07d6aab
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-18 22:49:03 +0530

    Added solution - LeetHub

---
+++ 704414-next-prime-number-GFG/704414-next-prime-number.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    static boolean isPrime(int n) {
+
+        if (n < 2) return false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int nextPrime(int n) {
+
+        int num = n + 1;
+
+        while (!isPrime(num)) {
+            num++;
+        }
+
+        return num;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
