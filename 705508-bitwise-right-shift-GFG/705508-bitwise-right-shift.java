40d11086851af39928f23e6826a563e77274be98
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-19 23:38:13 +0530

    Added solution - LeetHub

---
+++ 705508-bitwise-right-shift-GFG/705508-bitwise-right-shift.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // code here
+        
+        System.out.print(a >> b);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
