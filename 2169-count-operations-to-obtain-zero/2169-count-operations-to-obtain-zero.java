d6ae14702f90f9d0e1896539846248b8df5b32be
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-20 22:51:30 +0530

    Time: 1 ms (84.35%), Space: 42 MB (92.35%) - LeetHub - Time: O(log(min(num1, num2))), Space: O(1)

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int count = 0;
+
+        if(num1 == 0 && num2 == 0){
+            return 0;
+        }
+
+        if(num1 == num2){
+            return 1;
+        }
+
+        while(num1 > 0 && num2 > 0){
+            if(num1 < num2){
+                num2 = num2 - num1;
+            }
+            else{
+                num1 = num1 - num2;
+            }
+            count++;
+
+        }
+
+        return count;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
