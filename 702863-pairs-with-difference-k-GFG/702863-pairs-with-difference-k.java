ca4ce063582f715068208c4e218226f3e45e93c1
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-21 18:54:27 +0530

    Added solution - LeetHub

---
+++ 702863-pairs-with-difference-k-GFG/702863-pairs-with-difference-k.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+class Solution {
+    int countPairs(int[] arr, int k) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int num : arr) {
+
+            count += map.getOrDefault(num - k, 0);
+
+            if (k != 0) {
+                count += map.getOrDefault(num + k, 0);
+            }
+
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
