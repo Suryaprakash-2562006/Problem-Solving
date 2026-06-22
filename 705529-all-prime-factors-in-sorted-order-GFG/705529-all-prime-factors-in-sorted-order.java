597062840dbaf0394e928876a4858ef483c3ec19
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-22 21:58:53 +0530

    Added solution - LeetHub

---
+++ 705529-all-prime-factors-in-sorted-order-GFG/705529-all-prime-factors-in-sorted-order.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static void printPrimeFactorization(int n) {
+
+        for (int i = 2; i <= n; i++) {
+
+            while (n % i == 0) {
+                System.out.print(i + " ");
+                n = n / i;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
