a020e12a32137788db74a109558cc82f8ed58197
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-24 21:50:19 +0530

    Added solution - LeetHub

---
+++ 703092-two-sum-pair-with-given-sum-GFG/703092-two-sum-pair-with-given-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+    
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : arr){
+            int need = target - num;
+            
+            if(set.contains(need)){
+                return true;
+            }
+            set.add(num);
+        }
+        
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
