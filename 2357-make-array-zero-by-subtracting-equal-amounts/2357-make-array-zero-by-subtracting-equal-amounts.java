8583080ec0b28aaf82c360c71acc5e71840f490e
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-26 21:09:32 +0530

    Time: 1 ms (93.79%), Space: 43.1 MB (38.01%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+
+        int count = 0;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                continue;
+            }
+            else{
+                set.add(nums[i]);
+            }
+        }
+
+        for(int x : set){
+            if(x != 0){
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
