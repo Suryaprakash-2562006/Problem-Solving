f7b5924df93fcb485604b4765d313764b684b912
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-30 19:13:30 +0530

    Time: 0 ms (100%), Space: 43.8 MB (33.33%) - LeetHub - Time: O(n), Space: O(1)

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -1,24 +1,38 @@
 class Solution {
     public int countPartitions(int[] nums) {
 
-        int sum = 0;
+        // int sum = 0;
         
-        int count = 0;
+        // int count = 0;
 
-        for(int i=0;i<nums.length-1;i++){
-            sum += nums[i];
-            int remSum = 0;
+        // for(int i=0;i<nums.length-1;i++){
+        //     sum += nums[i];
+        //     int remSum = 0;
 
-            for(int j=i+1;j<nums.length;j++){
-                remSum += nums[j];
-            }
+        //     for(int j=i+1;j<nums.length;j++){
+        //         remSum += nums[j];
+        //     }
 
-            if(Math.abs(sum - remSum) % 2 == 0){
-                count++;
-            }
+        //     if(Math.abs(sum - remSum) % 2 == 0){
+        //         count++;
+        //     }
+        // }
+
+        // return count;
+
+
+
+        int total = 0;
+
+        for(int num : nums) {
+            total += num;
         }
 
-        return count;
+        if(total % 2 == 0)
+            return nums.length - 1;
+
+        return 0;
+ 
 
         
         
