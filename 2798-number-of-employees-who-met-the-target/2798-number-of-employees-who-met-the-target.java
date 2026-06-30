927ec70ab9bd36e63c7489441957911940d0fd59
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-30 22:12:21 +0530

    Time: 0 ms (100%), Space: 44.2 MB (41.69%) - LeetHub - Time: O(?), Space: O(?)

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -1,9 +1,9 @@
 class Solution {
     public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
-        int n = hours.length;
+
         int count = 0;
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<hours.length;i++){
             if(hours[i] >= target) count++;
         }
 
