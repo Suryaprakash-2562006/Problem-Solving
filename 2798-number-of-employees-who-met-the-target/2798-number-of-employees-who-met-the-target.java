2a96566f9d085ada39f78c3e1e43a87a23ef30b8
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-06-30 22:10:39 +0530

    Time: 0 ms (100%), Space: 44.2 MB (22.66%) - LeetHub - Time: O, Space: O(?)

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+
+        int count = 0;
+
+        for(int i=0;i<hours.length;i++){
+            if(hours[i] >= target) count++;
+        }
+
+        return count;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
