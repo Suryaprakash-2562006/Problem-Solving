61be8baf19666c91837705dbe6d45e953b4428bc
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-01 09:58:13 +0530

    Time: 3 ms (87.51%), Space: 43.1 MB (85.34%) - LeetHub - Time: O(n), Space: O(n)

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,57 @@
 class Solution {
     public boolean isValid(String s) {
 
-        while(true){
-            if(s.contains("()")){
-                s = s.replace("()" , "");
+        // while(true){
+        //     if(s.contains("()")){
+        //         s = s.replace("()" , "");
+        //     }
+        //     else if(s.contains("{}")){
+        //         s = s.replace("{}","");
+        //     }
+        //     else if(s.contains("[]")){
+        //         s = s.replace("[]","");
+        //     }
+        //     else{
+        //         return s.isEmpty();
+        //     }
+        // }
+
+
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // Opening brackets
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
             }
-            else if(s.contains("{}")){
-                s = s.replace("{}","");
-            }
-            else if(s.contains("[]")){
-                s = s.replace("[]","");
-            }
-            else{
-                return s.isEmpty();
+
+            // Closing brackets
+            else {
+
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(')
+                    return false;
+
+                if (ch == '}' && top != '{')
+                    return false;
+
+                if (ch == ']' && top != '[')
+                    return false;
             }
         }
-        
+
+        return stack.isEmpty();
     }
 }
+        
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
