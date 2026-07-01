5698eb700f8ef50cea96778d0c7e4ab6c03929f8
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-01 20:47:03 +0530

    Added solution - LeetHub

---
+++ 701220-majority-element-GFG/701220-majority-element.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    static int majorityElement(int arr[]) {
+
+        int candidate = 0;
+        int count = 0;
+
+        // Find candidate
+        for (int num : arr) {
+
+            if (count == 0) {
+                candidate = num;
+                count = 1;
+            } else if (candidate == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        // Verify candidate
+        count = 0;
+
+        for (int num : arr) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return count > arr.length / 2 ? candidate : -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
