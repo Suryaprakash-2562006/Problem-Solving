2ede84d83d03ef228ba4ab7cff86dcd4373e97f9
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-01 23:06:33 +0530

    Added solution - LeetHub

---
+++ 704151-java-basic-data-types-GFG/704151-java-basic-data-types.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    int javaIntType(Scanner sc) {
+        return sc.nextInt();
+    }
+
+    String javaStringType(Scanner sc) {
+        return sc.next();
+    }
+
+    float javaFloatType(Scanner sc) {
+        return sc.nextFloat();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
