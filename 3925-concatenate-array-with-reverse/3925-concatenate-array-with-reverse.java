748aa0e4610323e9637ee9c4e8d0583d3473bd6e
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-03 23:44:05 +0530

    Time: 1 ms (99.99%), Space: 47.2 MB (87.88%) - LeetHub - Time: O(n), Space: O(n)

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.java
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.java
@@ -1,16 +1,30 @@
 class Solution {
     public int[] concatWithReverse(int[] nums) {
+        // int n = nums.length;
+        // int[] ans = new int[2 * n];
+        // int i = 0;
+        // int j = ans.length-1;
+        // while(i<n){
+        //     ans[i] = nums[i];
+        //     ans[j] = nums[i];
+        //     i++;
+        //     j--;
+        // }
+        // return ans;
+
         int n = nums.length;
-        int[] ans = new int[2 * n];
-        int i = 0;
-        int j = ans.length-1;
-        while(i<n){
-            ans[i] = nums[i];
-            ans[j] = nums[i];
-            i++;
-            j--;
+        int ans[] = new int[n*2];
+        int j = 0;
+
+        for(int x : nums){
+            ans[j++] = x;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            ans[j++] = nums[i];
         }
         return ans;
+
     }
 }
 
