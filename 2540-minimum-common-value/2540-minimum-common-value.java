d248d90e1f67b72233919a3e4e10691ff06fdbde
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-07 20:39:51 +0530

    Time: 1 ms (97.46%), Space: 80.5 MB (48.95%) - LeetHub - Time: O(n + m), Space: O(1)

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -5,13 +5,12 @@ class Solution {
         int j=0;
 
         while(i<nums1.length && j<nums2.length){
-            if(nums1[i] == nums2[j]){
-                return nums1[i];
-                
-            }
-            else if(nums1[i] < nums2[j]) i++;
+            
+            if(nums1[i] < nums2[j]) i++;
 
-            else j++;
+            else if(nums1[i] > nums2[j]) j++;
+
+            else return nums1[i];
         }
 
         return -1;
