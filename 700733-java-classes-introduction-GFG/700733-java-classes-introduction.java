ecf5e1c770cd965dc469aa6203a54d23cec927b0
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-07 22:34:01 +0530

    Added solution - LeetHub

--- 700733-java-classes-introduction-GFG/700733-java-classes-introduction.java
+++ 700733-java-classes-introduction-GFG/700733-java-classes-introduction.java
@@ -1,26 +1,26 @@
 class task {
-    int length;
-    int width;
-    int height;
+    int l;
+    int w;
+    int h;
 
     public void set_length(int l) {
         // Add your code here.
-        length = l;
+        this.l = l;
     }
 
     public void set_width(int w) {
         // Add your code here.
-        width = w;
+        this.w = w;
     }
 
     public void set_height(int h) {
         // Add your code here.
-        height = h;
+        this.h = h;
     }
 
     public void volume() {
         // Add your code here.
-        System.out.println(length*width*height);
+        System.out.println(l*w*h);
     }
 }
 
