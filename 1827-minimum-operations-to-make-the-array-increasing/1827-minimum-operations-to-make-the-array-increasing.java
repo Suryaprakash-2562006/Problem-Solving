57f74b066de368eb950b666eda5ab7cfd96473f2
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-07 22:09:34 +0530

    Time: 5 ms (24.73%), Space: 47 MB (59.96%) - LeetHub - Time: O(?), Space: O(?)

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -1,6 +1,16 @@
 class Solution {
     public int minOperations(int[] nums) {
 
+        // int count = 0;
+
+        // for(int i=1;i<nums.length;i++){
+        //     while(nums[i] <= nums[i-1]){
+        //         nums[i]++;
+        //         count++;
+        //     }
+        // }
+        // return count;
+
         int count = 0;
 
         for(int i=1;i<nums.length;i++){
