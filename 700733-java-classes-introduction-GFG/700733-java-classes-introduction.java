2af957249fe2174aaf724162d8f1d13728ced0ec
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-07 22:32:43 +0530

    Added solution - LeetHub

---
+++ 700733-java-classes-introduction-GFG/700733-java-classes-introduction.java
@@ -0,0 +1,29 @@
+class task {
+    int length;
+    int width;
+    int height;
+
+    public void set_length(int l) {
+        // Add your code here.
+        length = l;
+    }
+
+    public void set_width(int w) {
+        // Add your code here.
+        width = w;
+    }
+
+    public void set_height(int h) {
+        // Add your code here.
+        height = h;
+    }
+
+    public void volume() {
+        // Add your code here.
+        System.out.println(length*width*height);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
