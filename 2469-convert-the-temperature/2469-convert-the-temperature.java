5fdb68fbf4a10e51e27ba726a5ea437ca9553884
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-07 21:09:35 +0530

    Time: 0 ms (100%), Space: 44 MB (100%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.java
+++ 2469-convert-the-temperature/2469-convert-the-temperature.java
@@ -1,7 +1,12 @@
 class Solution {
     public double[] convertTemperature(double celsius) {
 
-        return new double[] {celsius + 273.15 , celsius * 1.80 + 32.00};
+        //return new double[] {celsius + 273.15 , celsius * 1.80 + 32.00};
+
+        double kel=celsius + 273.15;
+        double fah=celsius * 1.80 + 32.00;
+        double[] arr= {kel,fah};
+        return arr;
         
     }
 }
