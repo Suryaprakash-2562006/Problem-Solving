7cb6bb465976b821b53a7354a6a97e0e192a470c
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-08 21:04:57 +0530

    Time: 1 ms (99.34%), Space: 45 MB (98.38%) - LeetHub

---
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstUniqueEven(int[] nums) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] % 2 != 0)
+                continue;
+
+            int count = 0;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+
+            if (count == 1) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
