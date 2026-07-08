7ec32612fc1d218ac403562d800550f7ef78aace
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-08 21:11:12 +0530

    Time: 1 ms (100%), Space: 45.5 MB (6.78%) - LeetHub - Time: O, Space: O(?)

--- 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.java
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.java
@@ -1,18 +1,16 @@
 class Solution {
     public int subarraySum(int[] nums) {
+        int res=0;
 
-        int total = 0;
-
-        for (int i = 0; i < nums.length; i++) {
-
-            int start = Math.max(0, i - nums[i]);
-
-            for (int j = start; j <= i; j++) {
-                total += nums[j];
+        for(int i=0;i<nums.length;i++){
+            int sum=0;
+            int st=Math.max(0, i-nums[i]);
+            for(int j=st;j<=i;j++){
+                sum+=nums[j];
             }
+            res+=sum;
         }
-
-        return total;
+        return res;
     }
 }
 
