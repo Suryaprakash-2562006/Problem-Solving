89506be1a0dde92fa963393eb7419f5d09a7c4fa
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-08 21:14:18 +0530

    Time: 1 ms (100%), Space: 45.4 MB (33.05%) - LeetHub - Time: O(n^2), Space: O(1)

--- 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.java
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.java
@@ -1,9 +1,7 @@
 class Solution {
     public int subarraySum(int[] nums) {
         int res=0;
-
         for(int i=0;i<nums.length;i++){
-            int sum=0;
             int st=Math.max(0, i-nums[i]);
             for(int j=st;j<=i;j++){
                 res+=nums[j];
@@ -13,6 +11,7 @@ class Solution {
         return res;
     }
 }
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
