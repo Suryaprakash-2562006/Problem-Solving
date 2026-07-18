cd55accf8b7731c1b804402a38f1f030616cb48a
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-18 20:58:16 +0530

    Added solution - Time: O(?), Space: O(1)

---
+++ 703946-java-operators-relational-set-2-GFG/703946-java-operators-relational-set-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    static void relationalOperators(int A, int B) {
+        // code here
+        
+        if(A > B) System.out.println(A + " is greater than " + B);
+        else System.out.println(A + " is less than " + B);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
