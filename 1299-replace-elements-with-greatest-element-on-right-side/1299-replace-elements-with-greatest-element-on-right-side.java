7c4e1bac639ad576be66657660e9e87af69f277e
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-24 21:09:19 +0530

    Time: 2 ms (99.54%), Space: 48.6 MB (98.68%) - LeetHub - Time: O(n), Space: O(1)

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,14 +1,23 @@
 class Solution {
+
     public int[] replaceElements(int[] arr) {
+
         int max = -1;
+
         for (int i = arr.length - 1;i >= 0;i--) {
+
             int cu = arr[i];
+
             arr[i] = max;
+
             max = Math.max(max, cu);
+
         }
 
         return arr;
+
     }
+
 }
 
 // Synced seamlessly with LeetHub Pro
