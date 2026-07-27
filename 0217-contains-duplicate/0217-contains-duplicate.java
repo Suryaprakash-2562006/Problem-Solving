1952519b7eb6d0454b7b5cc07435a0bccdc5ff82
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-07-27 21:36:33 +0530

    Time: 24 ms (26.87%), Space: 93.1 MB (74.02%) - LeetHub - Time: O(n), Space: O(n)

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,13 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> s = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-            if(s.contains(nums[i])){
-                return true;
-            }
-            s.add(nums[i]);
-        }
-        return false;
+        // HashSet<Integer> s = new HashSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     if(s.contains(nums[i])){
+        //         return true;
+        //     }
+        //     s.add(nums[i]);
+        // }
+        // return false;
         /*
         HashSet<Integer> s = new HashSet<>();
         for(int num : nums){
@@ -19,9 +19,18 @@ class Solution {
         return false;
 
         */
-        
+
+
+
+        HashSet<Integer> a = new HashSet<>();
+        for(int num : nums) {
+            a.add(num);
+        }
+        if(a.size() == nums.length) return false;
+        return true;
     }
 }
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
