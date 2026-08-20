be61227d647bb75860e8f3a43dddb414ddd9e4a7
Suryaprakash-2562006 <suryaprakash2562006@gmail.com>
2026-08-20 21:46:12 +0530

    Time: 7 ms (12.33%), Space: 45.2 MB (52.8%) - LeetHub

---
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addedInteger(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int ans = nums2[0]-nums1[0];
+
+        return ans;
+
+
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
